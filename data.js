1ab040e075cb33789f85f6ddb4235a5b7d506011
A-simple-dream <77008190+A-simple-dream@users.noreply.github.com>
2025-08-01 00:55:32 +0700

    data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+window.dataFromSubdomain = {
+  data: {
+    nameBook: "Cùng Nhau Đi Qua Mùa Hạ",
+    pageInfos: [
+      { text: "Mùa hè rực rỡ của chúng ta", image: "photo/photo1.jpg" },
+      { text: "Em và biển", image: "photo/photo2.jpg" },
+      { text: "", image: "photo/photo3.jpg" },
+      { text: "27-07-2025", image: "photo/photo4.jpg" },
+      { text: "", image: "photo/photo5.jpg" },
+      { text: "", image: "photo/photo6.jpg" }
+    ]
+  }
+};
